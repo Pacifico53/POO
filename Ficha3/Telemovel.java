d8cf60022e510bd32ba34092485cac400038be13
Pacifico53 <bernardo2viseu@gmail.com>
2018-03-01 15:49:46 +0000

    iniciada class telemovel

---
+++ Ficha3/Telemovel.java
@@ -0,0 +1,39 @@
+public class Telemovel
+{
+    private String marca;
+    private String modelo;
+    private int resX;
+    private int resY;
+    private int armazenamentoSMS;
+    private int armazenamentoFotos;
+    private int armazenamentoApps;
+    private int armazenamentoTotal;
+    private int espacoOcupado;
+    private int numeroFotos;
+    private int numeroApps;
+    private String[] nomeApps;
+    /**
+     * Constructor por omissao
+     */
+    public Telemovel()
+    {
+        this.marca = "";
+        this.modelo = "";
+        this.resX = 0;
+        this.resY = 0;
+        this.armazenamentoSMS = 0;
+        this.armazenamentoFotos = 0;
+        this.armazenamentoApps = 0;
+        this.armazenamentoTotal = 0;
+        this.espacoOcupado = 0;
+        this.numeroFotos = 0;
+        this.numeroApps = 0;
+        this.nomeApps = new String[0];
+    }
+    /**
+     * Construtor parameterizado
+     */
+    public Telemovel(String marca1, String modelo, int resXX, int resYY, int armazenamentoSMS, int armazenamentoFotos, int armazenamentoApps, int espacoOcupado; int nmroFotos, int nmroApps, String[] nomeApps){
+    
+    }
+}
